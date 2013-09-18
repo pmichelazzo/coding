ef100e23508d50b12dee035bc4073ff490cb80e7
Paulino Michelazzo <michelazzo@me.com>
2013-09-18 11:52:58 -0300

    Início dev logos

---
+++ logos/logos.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Logos
+ * @version 0.1
+ */
+/*
+Plugin Name: Logos
+Plugin URI: 
+Description: Plugin para carregamento de logomarcas em páginas do website.
+Author: Paulino Michelazzo
+Version: 0.1
+Author URI: http://www.michelazzo.com.br
+*/
+
+// Carga do CSS
+function logos_css() {
+	echo "
+	<style type='text/css'>
+	#logos {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'logos_css' );
+
+?>
